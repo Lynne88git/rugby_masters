83d2c76d8b0195201c237722e01b8ad2ca81731c
geldenhuys.lynne@gmail.com <geldenhuys.lynne@gmail.com>
2022-04-12 16:35:38 +0200

    base setup complete

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './app.styles.scss'
+
+export default function App() {
+  return (
+    <div className="app-container">
+      Webpack 5 boilerplate for react using babel, sass, with a hot dev server and an optimized
+      production build.
+    </div>
+  )
+}
